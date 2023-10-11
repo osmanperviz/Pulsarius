5253d47015160044564052af4bdd21bbcb04ed44
Osman Perviz <osman.perviz@studitemps.de>
2023-10-11 14:30:07 +0200

    remove routes condition.

--- lib/pulsarius_web/router.ex
+++ lib/pulsarius_web/router.ex
@@ -41,61 +41,59 @@ defmodule PulsariusWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through :browser
+  scope "/" do
+    pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: PulsariusWeb.Telemetry
+    live_dashboard "/dashboard", metrics: PulsariusWeb.Telemetry
 
-      live_session :invite, root_layout: {PulsariusWeb.LayoutView, :invite} do
-        live "/invitation/:id", PulsariusWeb.InvitationLive.New, :new
-        live "/invitation/join/:account_id", PulsariusWeb.InvitationLive.Join, :join
-      end
+    live_session :invite, root_layout: {PulsariusWeb.LayoutView, :invite} do
+      live "/invitation/:id", PulsariusWeb.InvitationLive.New, :new
+      live "/invitation/join/:account_id", PulsariusWeb.InvitationLive.Join, :join
+    end
 
-      live_session :users, on_mount: [PulsariusWeb.AuthAssigns, PulsariusWeb.RouteAssigns] do
-        live "/monitors", PulsariusWeb.MonitorLive.Index, :index
-        live "/monitors/new", PulsariusWeb.MonitorLive.New, :new
-        live "/monitors/:id/edit", PulsariusWeb.MonitorLive.Edit, :edit
-        live "/monitors/:id", PulsariusWeb.MonitorLive.Show, :show
+    live_session :users, on_mount: [PulsariusWeb.AuthAssigns, PulsariusWeb.RouteAssigns] do
+      live "/monitors", PulsariusWeb.MonitorLive.Index, :index
+      live "/monitors/new", PulsariusWeb.MonitorLive.New, :new
+      live "/monitors/:id/edit", PulsariusWeb.MonitorLive.Edit, :edit
+      live "/monitors/:id", PulsariusWeb.MonitorLive.Show, :show
 
-        live "/monitors/:id/incidents", PulsariusWeb.IncidentsLive.Index, :index
-        live "/account/:id/incidents", PulsariusWeb.IncidentsLive.Account, :index
+      live "/monitors/:id/incidents", PulsariusWeb.IncidentsLive.Index, :index
+      live "/account/:id/incidents", PulsariusWeb.IncidentsLive.Account, :index
 
-        live "/monitors/:monitor_id/incidents/:incident_id",
-             PulsariusWeb.IncidentsLive.Show,
-             :show
+      live "/monitors/:monitor_id/incidents/:incident_id",
+           PulsariusWeb.IncidentsLive.Show,
+           :show
 
-        live "/users", PulsariusWeb.UserLive.Index, :index
-        live "/users/new", PulsariusWeb.UserLive.Index, :new
-        live "/users/:id/edit", PulsariusWeb.UserLive.Index, :edit
-        live "/users/:id", PulsariusWeb.UserLive.Show, :show
-        live "/users/:id/show/edit", PulsariusWeb.UserLive.Show, :edit
+      live "/users", PulsariusWeb.UserLive.Index, :index
+      live "/users/new", PulsariusWeb.UserLive.Index, :new
+      live "/users/:id/edit", PulsariusWeb.UserLive.Index, :edit
+      live "/users/:id", PulsariusWeb.UserLive.Show, :show
+      live "/users/:id/show/edit", PulsariusWeb.UserLive.Show, :edit
 
-        live "/billing/pricing", PulsariusWeb.SubscriptionLive.Pricing, :pricing
-        live "/billing/subscription", PulsariusWeb.SubscriptionLive.New, :new
-        live "/billing/change_subscription", PulsariusWeb.SubscriptionLive.Edit, :edit
+      live "/billing/pricing", PulsariusWeb.SubscriptionLive.Pricing, :pricing
+      live "/billing/subscription", PulsariusWeb.SubscriptionLive.New, :new
+      live "/billing/change_subscription", PulsariusWeb.SubscriptionLive.Edit, :edit
 
-        live "/integrations", PulsariusWeb.IntegrationsLive.Index, :index
-        live "/integrations/slack/new", PulsariusWeb.IntegrationsLive.Slack.New, :new
-        live "/integrations/slack", PulsariusWeb.IntegrationsLive.Slack.Index, :index
+      live "/integrations", PulsariusWeb.IntegrationsLive.Index, :index
+      live "/integrations/slack/new", PulsariusWeb.IntegrationsLive.Slack.New, :new
+      live "/integrations/slack", PulsariusWeb.IntegrationsLive.Slack.Index, :index
 
-        live "/integrations/teams/new", PulsariusWeb.IntegrationsLive.Teams.New, :new
-        live "/integrations/teams", PulsariusWeb.IntegrationsLive.Teams.Index, :index
-      end
+      live "/integrations/teams/new", PulsariusWeb.IntegrationsLive.Teams.New, :new
+      live "/integrations/teams", PulsariusWeb.IntegrationsLive.Teams.Index, :index
     end
   end
+end
 
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
+# Enables the Swoosh mailbox preview in development.
+#
+# Note that preview only shows emails that were sent by the same
+# node running the Phoenix server.
+if Mix.env() == :dev do
+  scope "/dev" do
+    pipe_through :browser
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    forward "/mailbox", Plug.Swoosh.MailboxPreview
   end
 end
