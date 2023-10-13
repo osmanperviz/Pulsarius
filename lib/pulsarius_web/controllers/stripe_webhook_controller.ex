00d47aa6c23a4265a8c387907934a9eeb69df253
Osman Perviz <osman.perviz@studitemps.de>
2023-10-13 11:27:09 +0200

    uncomment plug.

--- lib/pulsarius_web/controllers/stripe_webhook_controller.ex
+++ lib/pulsarius_web/controllers/stripe_webhook_controller.ex
@@ -7,7 +7,7 @@ defmodule PulsariusWeb.StripeWebhookController do
   @webhook_signing_key Application.get_env(:stripity_stripe, :webhook_signing_key)
   @stripe_service Application.get_env(:live_view_stripe, :stripe_service)
 
-  # plug(:assert_body_and_signature)
+  plug(:assert_body_and_signature)
 
   @doc """
   Construct a stripe event, if it is valid we notify subscribers.
@@ -42,7 +42,7 @@ defmodule PulsariusWeb.StripeWebhookController do
 
       _ ->
         conn
-        |> send_resp(:created, "")
+        |> put_status(500)
         |> halt()
     end
   end
