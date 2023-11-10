86987fc022d0086484a8dac0dfcb27d5e99e6531
Osman Perviz <osman.perviz@studitemps.de>
2023-11-10 15:32:27 +0100

    add new signing key.

--- lib/pulsarius_web/controllers/stripe_webhook_controller.ex
+++ lib/pulsarius_web/controllers/stripe_webhook_controller.ex
@@ -18,7 +18,9 @@ defmodule PulsariusWeb.StripeWebhookController do
     case Stripe.Webhook.construct_event(
            conn.assigns[:raw_body],
            conn.assigns[:stripe_signature],
-           "whsec_RXmak4kvwYK34ThAXXJc3GAA2MOXIigB"
+           "whsec_8YxTl2HVrRU4Zn0WHFdbdS1hoLTD3vSg"
+          #  "whsec_d2a82c11224e8f58fb3df1dc685b10b360646395bdb6a9fd159346fe43f84ee3"
+          #  "whsec_RXmak4kvwYK34ThAXXJc3GAA2MOXIigB"
            # @webhook_signing_key
          ) do
       {:ok, %{} = event} ->
